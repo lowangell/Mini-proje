a425f3c44da64a6f8baa4cc1c767c9b85f1145a9
lowangell <sila.karahan3401@gmail.com>
2024-07-07 22:39:38 +0300

    Mini-Proje
    Kullanıcı Bilgileri

---
+++ AdresDefteri/Adres Defteri/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Adres_Defteri;
+
+
+{
+    AdressInfo adres = new AdressInfo();
+    adres.StreetName = "Centilmen";
+    adres.District = "Bahcelievler";
+    adres.City = "Istanbul";
+    adres.ApartmentNo = 49;
+    adres.Country = "Turkey";
+    adres.UserName = "Sila";
+    adres.PhoneNumber = "1234567890";
+    adres.HomePhone = "12345678342";
+    adres.userInformation();
+}
---
+++ AdresDefteri/Adres Defteri/User.cs
@@ -0,0 +1,37 @@
+﻿using Adres_Defteri;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adres_Defteri
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HomePhone { get; set; }
+        public virtual void userInformation () {
+            Console.WriteLine($"Kullanicinin ismi {UserName}");
+            Console.WriteLine($"Kullanicinin cep numarasi {PhoneNumber}");
+            Console.WriteLine($"Kullanicinin ev telefonu {HomePhone}");
+        }
+    }
+}
+public class AdressInfo : User
+{
+    public string City;
+    public string Country;
+    public int ApartmentNo;
+    public string StreetName;
+    public string District;
+
+    public override void userInformation ()
+    {
+        Console.WriteLine($"Kullanicinin ismi {UserName}");
+        Console.WriteLine($"Kullanicinin cep numarasi {PhoneNumber}");
+        Console.WriteLine($"Kullanicinin ev telefonu {HomePhone}");
+        Console.WriteLine($"Adres: {City} No: {ApartmentNo}, {Country}, {District}, {StreetName}");
+    }
+}
